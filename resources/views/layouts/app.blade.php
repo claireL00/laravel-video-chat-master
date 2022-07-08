e1bba0f88092c5d8c7222814f5527b14817f8881
claireL00 <20191316@s.ubaguio.edu>
2022-07-08 19:30:43 +0800

    pls gumana ka

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,10 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    @yield('scripts')
+    <meta name="description" content="Build A Scalable Video Chat Application With Agora" />
+    <meta name="keywords" content="Video Call, Agora, Laravel, Real Time Engagement" />
+    <meta name="author" content="Kofi Obrasi Ocran" />
+    <script src="https://cdn.agora.io/sdk/release/AgoraRTCSDK-3.3.1.js"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
